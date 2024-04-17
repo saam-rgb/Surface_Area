4afd018e6a0f3c69731b92f2244067910c654eb6
saam <saamsallvin@gmail.com>
2024-04-17 10:07:20 +0530

    cone and cylinder added

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
+// Cube functions
 function cubeTSA() {
-  var givenSide = document.getElementById("side").value;
+  let givenSide = document.getElementById("side").value;
   console.log(givenSide);
-  var TSA = 6 * givenSide * givenSide;
+  let TSA = 6 * givenSide * givenSide;
 
   console.log(TSA);
-  document.getElementById("solution").innerHTML = TSA;
+  document.getElementById("cubeSol").innerHTML = TSA;
 }
 const cubeCSA = () => {
-  var givenSide = document.getElementById("side").value;
-  var CSA = 4 * givenSide * givenSide;
-  document.getElementById("solution").innerHTML = CSA;
+  let givenSide = document.getElementById("side").value;
+  let CSA = 4 * givenSide * givenSide;
+  document.getElementById("cubeSol").innerHTML = CSA;
 };
 const cubeVol = () => {
-  var givenSide = document.getElementById("side").value;
-  var CSA = givenSide * givenSide * givenSide;
-  document.getElementById("solution").innerHTML = CSA;
+  let givenSide = document.getElementById("side").value;
+  let Vol = givenSide * givenSide * givenSide;
+  document.getElementById("cubeSol").innerHTML = Vol;
+};
+
+//Cone functions
+const coneTSA = () => {
+  let baseRadius = document.getElementById("baseRadius").value;
+  let coneHeight = document.getElementById("coneHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${coneHeight}`);
+  let TSA = 3.14 * baseRadius * (baseRadius + coneHeight);
+  document.getElementById("coneSol").innerHTML = TSA;
+};
+
+const coneCSA = () => {
+  let baseRadius = document.getElementById("baseRadius").value;
+  let coneHeight = document.getElementById("coneHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${coneHeight}`);
+  let CSA = 3.14 * baseRadius * coneHeight;
+  document.getElementById("coneSol").innerHTML = CSA;
+};
+
+const coneVol = () => {
+  let baseRadius = document.getElementById("baseRadius").value;
+  let coneHeight = document.getElementById("coneHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${coneHeight}`);
+  let Vol = (3.14 * baseRadius * baseRadius * coneHeight) / 3;
+  document.getElementById("coneSol").innerHTML = Vol;
+};
+
+// Cylinder functions
+
+const cylinderTSA = () => {
+  let baseRadius = document.getElementById("radius").value;
+  let cylinderHeight = document.getElementById("cylinderHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${cylinderHeight}`);
+  let TSA = 3.14 * baseRadius * (baseRadius + cylinderHeight);
+  document.getElementById("cylinderSol").innerHTML = TSA;
+};
+
+const cylinderCSA = () => {
+  let baseRadius = document.getElementById("radius").value;
+  let cylinderHeight = document.getElementById("cylinderHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${cylinderHeight}`);
+  let CSA = 3.14 * baseRadius * cylinderHeight;
+  document.getElementById("cylinderSol").innerHTML = CSA;
+};
+
+const cylinderVol = () => {
+  let baseRadius = document.getElementById("radius").value;
+  let cylinderHeight = document.getElementById("cylinderHeight").value;
+  console.log(`Base radius : ${baseRadius} and Height ${cylinderHeight}`);
+  let Vol = 3.14 * baseRadius * baseRadius * cylinderHeight;
+  document.getElementById("cylinderSol").innerHTML = Vol;
 };
 
 // tsa = document.getElementById("tsa");
